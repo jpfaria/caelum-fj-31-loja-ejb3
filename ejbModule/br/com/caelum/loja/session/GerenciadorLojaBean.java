f4ad1e949a8ad47b2a823b605374d92ec0c1c170
João Paulo Faria <jpfaria@gmail.com>
2013-06-26 00:18:02 -0300

    .

--- ejbModule/br/com/caelum/loja/session/GerenciadorLojaBean.java
+++ ejbModule/br/com/caelum/loja/session/GerenciadorLojaBean.java
@@ -4,43 +4,52 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
 import javax.ejb.Remote;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionManagement;
+import javax.ejb.TransactionManagementType;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.UserTransaction;
 
 import br.com.caelum.loja.entity.Autor;
 import br.com.caelum.loja.entity.Livro;
 
+@TransactionManagement(TransactionManagementType.BEAN)
 @Stateless
 @Remote(GerenciadorLoja.class)
 public class GerenciadorLojaBean implements GerenciadorLoja {
 
+	@Resource
+	private UserTransaction ut;
+	
 	@PersistenceContext
 	private EntityManager manager;
-	
+
 	private Map<String, Livro> repositorio;
-	
+
 	public GerenciadorLojaBean() {
 		this.repositorio = new HashMap<String, Livro>();
-		
+
 		Livro l1 = new Livro();
 		l1.setNome("Pais e Filhos");
 		l1.setPreco(100.0);
-		
+
 		Livro l2 = new Livro();
 		l2.setNome("Noites brancas");
 		l2.setPreco(200.0);
-		
+
 		this.repositorio.put("1111", l1);
 		this.repositorio.put("2222", l2);
 	}
-	
+
 	@Override
 	public Livro procura(String isbn) {
 		return this.repositorio.get(isbn);
 	}
-	
+
 	@Override
 	public Livro procura(Long id) {
 		Livro livro = this.manager.find(Livro.class, id);
@@ -54,7 +63,38 @@ public class GerenciadorLojaBean implements GerenciadorLoja {
 		System.out.println("Livro salvo! ID: " + livro.getId());
 		return livro;
 	}
-
+	/*
+	public void salva(Livro livro) {
+		this.manager.persist(livro);
+		System.out.println("Livro salvo! ID: " + livro.getId());
+		throw new SalvaLivroException();
+	}
+	*/
+	/*
+	public void salva(Livro livro) {
+		try {
+			this.ut.begin();
+		}catch(Exception e) {
+			throw new EJBException(e);
+		}
+		
+		this.manager.persist(livro);
+		System.out.println("Livro salvo! ID: " + livro.getId());
+		
+		try {
+			this.ut.commit();
+		}catch(Exception e) {
+			try {
+				this.ut.rollback();
+			}catch(Exception e1) {
+				throw new EJBException(e);
+			}
+			throw new EJBException(e);
+		}
+		
+	}
+	*/
+	
 	@Override
 	public Autor salva(Autor autor) {
 		this.manager.persist(autor);
@@ -69,4 +109,17 @@ public class GerenciadorLojaBean implements GerenciadorLoja {
 		return l.getAutores();
 	}
 
+	@Override
+	public List<Livro> buscaLivrosPeloNomeDoAutor(String nome) {
+		System.out.println("Buscando livros pelo autor: " + nome);
+		
+		String jpql = "select livro from Livro as livro"
+				+ " join fetch livro.autores as autor"
+				+ " where autor.nome like :busca";
+		
+		Query query = this.manager.createQuery(jpql);
+		query.setParameter("busca", "%" + nome + "%");
+		
+		return query.getResultList();
+	}
 }
